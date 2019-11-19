d173686169212866ca9fa17c07235a65e3b78921
KFransen <kjtfrans@avans.nl>
2019-11-19 11:45:15 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -6,26 +6,42 @@ project "NativeFileDialog"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+	files {"src/*.h",
 		"src/include/*.h",
-		"src/*.h",
-		"src/**.c",
-		"src/**.cpp"
-	}
+		"src/nfd_common.c",
+	 }
 	
 	includedirs
     {
-		"src",
         "src/include"
     }
 	
-	filter "system:windows"
-        systemversion "latest"
-		defines 
-		{ 
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+    filter "system:windows"
+      language "C++"
+      files {"src/nfd_win.cpp"}
+
+    filter {"action:gmake or action:xcode4"}
+      buildoptions {"-fno-exceptions"}
+
+    filter "system:macosx"
+      language "C"
+      files {"src/nfd_cocoa.m"}
+
+
+
+    filter {"system:linux", "options:linux_backend=gtk3"}
+      language "C"
+      files {"src/nfd_gtk.c"}
+      buildoptions {"`pkg-config --cflags gtk+-3.0`"}
+    filter {"system:linux", "options:linux_backend=zenity"}
+      language "C"
+      files {"src/nfd_zenity.c"}
+
+
+    -- visual studio filters
+    filter "action:vs*"
+      defines { "_CRT_SECURE_NO_WARNINGS" }      
+
 
     filter "configurations:Debug"
         runtime "Debug"
@@ -33,4 +49,4 @@ project "NativeFileDialog"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "on"
+		optimize "on"
